9d1f093fd4c16cf65a7884747cafc32ee24d04b4
Markemp <ggerber@gmail.com>
2018-09-22 08:37:35 -0700

    Added tests project.  Untested fix for timing 919 c hunk.

---
+++ Cgf-Converter/CryEngine_Core/Chunks/ChunkTimingFormat_919.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CgfConverter.CryEngine_Core.Chunks
+{
+    public class ChunkTimingFormat_919 : ChunkTimingFormat
+    {
+        public override void Read(BinaryReader reader)
+        {
+            base.Read(reader);
+
+            // TODO:  This is copied from 918 but may not be entirely accurate.  Not tested.
+            this.SecsPerTick = reader.ReadSingle();
+            this.TicksPerFrame = reader.ReadInt32();
+            this.GlobalRange.Name = reader.ReadFString(32);  // Name is technically a String32, but F those structs
+            this.GlobalRange.Start = reader.ReadInt32();
+            this.GlobalRange.End = reader.ReadInt32();
+        }
+
+    }
+}
